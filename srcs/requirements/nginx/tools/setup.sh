a71b4e5b4925a3c0ea74e153768bedad22548289
HakimHC <ahmedh90111@gmail.com>
2023-09-04 17:34:04 +0200

    Nginx container generates TLS certs and private key

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
+openssl genpkey -algorithm RSA -out /etc/nginx/ssl/private_key.pem
+
+openssl req -new -x509 -key /etc/nginx/ssl/private_key.pem -out /etc/nginx/ssl/cert.pem -days 365 << EOF
+$COUNTRY
+$PROVINCE
+$LOCALITY
+$ORGANIZATION
+$UNIT
+$COMMON_NAME
+$EMAIL_ADDR
+EOF
+
 nginx -g "daemon off;"
