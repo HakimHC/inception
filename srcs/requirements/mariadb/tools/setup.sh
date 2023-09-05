b9775300d54a5bf2133ed42a0c2b1ac74d698fd2
HakimHC <ahmedh90111@gmail.com>
2023-09-05 19:13:19 +0000

    Added package installations to Docker images and added wpcli configuration

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
-apk update
-apk add mariadb mariadb-client
-
 chown -R mysql: /var/lib/mysql
 chmod 777 /var/lib/mysql
 
@@ -13,15 +10,16 @@ sleep 1
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER "'$MYSQL_USER'"@'localhost' IDENTIFIED BY "'$MYSQL_PASSWORD'";
-GRANT ALL PRIVILEGES ON wordpress.* TO "'$MYSQL_USER'"@'localhost';
+CREATE USER $MYSQL_USER@"'%'" IDENTIFIED BY "'$MYSQL_PASSWORD'";
+GRANT ALL PRIVILEGES ON wordpress.* TO $MYSQL_USER@"'%'";
 FLUSH PRIVILEGES;
 USE $MYSQL_DATABASE;
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (name varchar(255));
 INSERT INTO Users VALUES ('hakim');
 INSERT INTO Users VALUES ('papito');
-EXIT;
 EOF
+
+echo ======== USER CREATION FINISHED BY NOW =========
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown && mysqld
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 
-apk add bash \
-        curl \
-        php-fpm \
-        php-phar \
-        php-mysqli \
-        mariadb-client
+mkdir /var/log/mysql
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download
+wp core download --path=/wordpress
+
+
+wp config create --path=/wordpress --dbname=$MYSQL_DATABASE --dbuser=root --dbpass=dufus --dbhost=mariadb
+
+#wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --path=/wordpress
 
 tail -f /dev/null
