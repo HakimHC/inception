14d3d9d2df192e9291e3ee0cf965fb71ecc75570
HakimHC <ahmedh90111@gmail.com>
2023-09-06 19:51:16 +0000

    Changed mariadb start script

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,21 +5,21 @@ chmod 777 /var/lib/mysql
 
 mysql_install_db
 
-mysqld 
-sleep 1
+mysqld&
+sleep 2
 
-# mysql -u root -p$MYSQL_ROOT_PASSWORD -h localhost -P 3306 << EOF
-# CREATE DATABASE $MYSQL_DATABASE;
-# CREATE USER $MYSQL_USER@"'%'" IDENTIFIED BY "'$MYSQL_PASSWORD'";
-# GRANT ALL PRIVILEGES ON wordpress.* TO $MYSQL_USER@"'%'";
-# FLUSH PRIVILEGES;
-# USE $MYSQL_DATABASE;
-# DROP TABLE IF EXISTS Users;
-# CREATE TABLE Users (name varchar(255));
-# INSERT INTO Users VALUES ('hakim');
-# INSERT INTO Users VALUES ('papito');
-# EOF
-# 
-# echo ======== USER CREATION FINISHED BY NOW =========
+mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER $MYSQL_USER@"'%'" IDENTIFIED BY "'$MYSQL_PASSWORD'";
+GRANT ALL PRIVILEGES ON *.* TO $MYSQL_USER@"'%'" WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+USE $MYSQL_DATABASE;
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (name varchar(255));
+INSERT INTO Users VALUES ('hakim');
+INSERT INTO Users VALUES ('papito');
+EOF
 
-#mysqladmin -u root -p$MYSQL_ROOT_PASSWORD -h localhost -P 3306 shutdown && mysqld
+echo ======== USER CREATION FINISHED BY NOW =========
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown && mysqld
