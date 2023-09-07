af1996eee8b9bed2869fe0ffb97ddc55ecb6e133
HakimHC <ahmedh90111@gmail.com>
2023-09-07 17:46:42 +0000

    Wordpress is getting installed on the machine and nginx roots to /wordpress with index as index.php

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -7,10 +7,12 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+sleep 4
+
 wp core download --path=/wordpress
 
-#wp config create --path=/wordpress --dbname=$MYSQL_DATABASE --dbuser=root --dbpass=dufus --dbhost=mariadb
+wp config create --path=/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
 
-#wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --path=/wordpress
+wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --path=/wordpress
 
 tail -f /dev/null
