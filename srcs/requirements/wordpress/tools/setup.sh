e530d5c1c9efd7d17b2e8a73d738add2a5f2a331
HakimHC <ahmedh90111@gmail.com>
2023-09-12 15:06:23 +0200

    Added loop to wait for mariadb to start, and a static page

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,14 +1,29 @@
 #! /bin/sh
 
-mkdir /var/log/mysql
 
+check_connection() {
+  mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e "SELECT 1;" 2>/dev/null
+}
+
+#mkdir /var/log/mysql
+
+# install wordpress cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-sleep 10
+# connect to db
+while true; do
+  echo "Trying to connect to MYSQL..."
+  if check_connection; then
+    break
+  fi
+  sleep 1
+done
 
+echo "Connection successful."
+
+# Set up wordpress
 if [ ! -f /wordpress/wp-config.php ]; then
   wp core download --path=/wordpress
 
@@ -19,6 +34,12 @@ if [ ! -f /wordpress/wp-config.php ]; then
   wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --path=/wordpress
 
   wp theme install twentytwentytwo --activate --path=/wordpress
+fi
+
+# Set up my static page
+if [ ! -f "/wordpress/static-page/index.html" ]; then
+  mkdir -p /wordpress/static-page 2>/dev/null
+  cp ./tools/index.html /wordpress/static-page
 fi
 
 echo "Available WordPress themes:"
