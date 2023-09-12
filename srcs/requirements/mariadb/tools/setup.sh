fab7badf6e45c423db7817f3518d01b5ce66eb57
HakimHC <ahmedh90111@gmail.com>
2023-09-12 17:20:46 +0000

    Mysql daemon now has an init sql file to execute

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-function query {
-  mysql -u root -p$MYSQL_ROOT_PASSWORD -h localhost -e "$1"
+function add_query_line {
+  echo "$1" >> "$MYSQL_INIT_FILE"
 }
 
 chown -R mysql: /var/lib/mysql
@@ -9,25 +9,14 @@ chmod 777 /var/lib/mysql
 
 mysql_install_db
 
-mysqld&
-sleep 2
-#rc-service mariadb start
-
-check_database_query="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$MYSQL_DATABASE';"
-result=$(mysql -u root -p$MYSQL_ROOT_PASSWORD -h localhost -e "$check_database_query")
-
-echo hasta aqui
-
-if [ -z "$result" ]; then
-  query "CREATE DATABASE $MYSQL_DATABASE;"
-  query "CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-  query "CREATE USER $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-  query "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' WITH GRANT OPTION;"
-  query "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
-  query "FLUSH PRIVILEGES;"
-  echo done
-else
-  echo 42
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+  rm -f "$MYSQL_INIT_FILE"
+  add_query_line "CREATE DATABASE $MYSQL_DATABASE;"
+  add_query_line "CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+  add_query_line "CREATE USER $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+  add_query_line "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' WITH GRANT OPTION;"
+  add_query_line "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
+  add_query_line "FLUSH PRIVILEGES;"
 fi
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD -h localhost shutdown && mysqld
+mysqld --init-file=$MYSQL_INIT_FILE
