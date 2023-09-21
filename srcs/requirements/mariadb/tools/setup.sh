c0a91ebd2e8eeac29fec3f753799c89ada37cbf7
HakimHC <ahmedh90111@gmail.com>
2023-09-21 10:38:53 +0200

    Deleting root user and re-creating it with a password

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -7,7 +7,10 @@ function add_query_line {
 chown -R mysql: /var/lib/mysql
 chmod 777 /var/lib/mysql
 
+# tail -f /dev/null
+
 mysql_install_db
+# mysql_secure_installation
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
   rm -f "$MYSQL_INIT_FILE"
@@ -19,7 +22,11 @@ if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
   add_query_line "GRANT ALL PRIVILEGES ON $GRAFANA_DATABASE.* TO $MYSQL_USER@'%' WITH GRANT OPTION;"
   add_query_line "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
   add_query_line "FLUSH PRIVILEGES;"
-  mysqld --init-file=$MYSQL_INIT_FILE
+  add_query_line "DROP USER 'root'@'localhost';"
+  add_query_line "CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+  add_query_line "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+  add_query_line "FLUSH PRIVILEGES;"
+  mysqld_safe --init-file=$MYSQL_INIT_FILE
 else
-  mysqld
+  mysqld_safe
 fi
